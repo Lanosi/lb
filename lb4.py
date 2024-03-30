e8b78181ff308ee1f55c7f150961e1d7df84e102
Анимешка <artem.rozhnov.2005@mail.ru>
2024-03-30 16:34:48 +0300

    test11

--- lb4.py
+++ lb4.py
@@ -1,13 +1,32 @@
 import math
 
+# def main():
+#     try:
+#         a, b = 1 , 2.9
+#         print("Введите h")
+#         h = float(input())
+#         x = a
+#         k = int((b-a)/h)+1
+#         for i in range(k):
+#             y = 0.5 * x ** 2 - 1 - math.log10(abs(x - 3))
+#             print(x, y)
+#             x += h
+#
+#     except ValueError:
+#         print("Только числа")
+#         main()
+#
+# if __name__ == '__main__':
+#     main()
+
 def main():
     try:
         a, b = 1 , 2.9
         print("Введите h")
         h = float(input())
         x = a
-        k = int((b-a)/h)+1
-        for i in range(k):
+
+        while x <= b:
             y = 0.5 * x ** 2 - 1 - math.log10(abs(x - 3))
             print(x, y)
             x += h
