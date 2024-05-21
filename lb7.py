ff940622ecfe78ff34848f16caad3835eb841223
Анимешка <artem.rozhnov.2005@mail.ru>
2024-05-21 22:49:46 +0300

    7

--- lb7.py
+++ lb7.py
@@ -1,6 +1,8 @@
 import math
+from itertools import combinations
 
-#1
+
+# 1
 # a = 0.7
 # x = -2 * a
 # dx = a / 5
@@ -11,7 +13,7 @@ import math
 # if __name__ == '__main__':
 #     y(x)
 
-#2
+# 2
 # a = 3 * 10 ** 4
 # b = 6 * 10 ** 4
 # m = 4
@@ -21,5 +23,50 @@ import math
 #     f = k ** 3 - 25 * k ** 2 + 50 * k + 1000
 #     if t1 < f < t2 or f > m ** 8:
 #         print(f)
+#Показал
 
-#3
+# 3
+def main():
+    try:
+        print("Введите n значений X")
+        n = int(input())
+        print("Введите X нач")
+        Xstart = int(input())
+        print("Введите шаг HX")
+        Xstep = int(input())
+
+        print("Введите m значений Z")
+        m = int(input())
+        print("Введите Z нач")
+        Zstart = int(input())
+        print("Введите шаг HZ")
+        Zstep = int(input())
+
+        FinX = Xstart + n * Xstep
+        FinZ = Zstart + m * Zstep
+
+        Check = list()
+
+        for x in range(Xstart, FinX+1, Xstep):
+            for z in range(Zstart, FinZ+1, Zstep):
+                answer = (z / x + x / z - math.pi ** 0.5) / (0.5 + 10.7 ** 1 / 3 + math.atan(x))
+                print(answer)
+                Check.append(answer)
+
+        print(Check)
+    except ValueError:
+        print("Только целые числа")
+        main()
+
+
+if __name__ == '__main__':
+    main()
+
+#4
+# x = 0.1
+# while x < 10:
+#     if (x - 2) * x ** 1/3 == -1:
+#         print(x)
+#         break
+#     x += 0.1
+#     print((x - 2) * x ** 1/3)
