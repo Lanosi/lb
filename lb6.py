55b354b7370e6e8e3a91a0decbc44420bb1a7cfc
Анимешка <artem.rozhnov.2005@mail.ru>
2024-05-22 22:48:24 +0300

    6

--- lb6.py
+++ lb6.py
@@ -1,9 +1,11 @@
-#1
+import math
+
+# 1
 # for x in range(-500, 501):
 #     if abs(x) / 100 >= 1 and 200 <= abs(x) <= 299 or 400 <= abs(x) <= 499:
 #         print(x)
 
-#2
+# 2
 # N = int(input())
 # i1 = 1
 # i2 = 1
@@ -13,7 +15,15 @@
 #     i1 = fibI
 #     print(i2)
 
-#3
+# 3
 n = int(input())
-for i in range(1,n+1):
-    y = 1 / (abs()) ** 0.5
+
+
+def y(n):
+    if n < 3: return 0
+    if n >= 3: return 1 / (abs(y(n - 3) + math.sin(y(n - 1)) ** 2 + math.cos(y(n - 2)) ** 2)) ** 0.5
+
+print(y(n))
+
+    # for i in range(3,n+1):
+    #     y = 1 / (abs()) ** 0.5
